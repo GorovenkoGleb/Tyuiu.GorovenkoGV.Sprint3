9dbaa377dfe9c89067201c475ffc004f4188930e
GorovenkoGleb <gorovenkogv@outlook.com>
2023-10-29 08:54:34 +0500

    Task5

---
+++ Tyuiu.GorovenkoGV.Sprint3.Task5.V26/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GorovenkoGV.Sprint3.Task5.V26.Lib;
+
+namespace Tyuiu.GorovenkoGV.Sprint3.Task5.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Горовенко Г.В. | ПКТб 23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Вложенные циклы                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Горовенко Глеб Васильевич | ПКТб-23-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* sumSeries + (Math.Pow(x, k) + Math.Sin(k) при x = 2                     *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 2;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 13;
+
+            Console.WriteLine("Переменная X = " + x);
+            Console.WriteLine("Старт шага для первой суммы ряда = " + startValue1);
+            Console.WriteLine("Конец шага для первой суммы ряда = " + stopValue1);
+            Console.WriteLine("Старт шага для второй суммы ряда = " + startValue2);
+            Console.WriteLine("Конец шага для второй суммы ряда = " + stopValue2);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+
+
+            Console.ReadKey();
+        }
+    }
+}
