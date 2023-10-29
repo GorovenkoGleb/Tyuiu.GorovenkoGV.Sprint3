fdd80a4efca4a7eb4c3e9df3f99e30a7f673aa71
GorovenkoGleb <gorovenkogv@outlook.com>
2023-10-29 09:11:31 +0500

    Task6

---
+++ Tyuiu.GorovenkoGV.Sprint3.Task6.V30.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GorovenkoGV.Sprint3.Task6.V30.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GorovenkoGV.Sprint3.Task6.V30.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e750a4f4-8c75-41d7-856f-bdb4468ced85")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
