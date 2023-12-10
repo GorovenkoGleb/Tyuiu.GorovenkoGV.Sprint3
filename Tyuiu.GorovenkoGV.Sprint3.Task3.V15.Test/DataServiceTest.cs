bab72c6b6030a394f86dbade39184d49ed801e98
GorovenkoGleb <gorovenkogv@outlook.com>
2023-12-11 00:08:02 +0500

    task 3

---
+++ Tyuiu.GorovenkoGV.Sprint3.Task3.V15.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GorovenkoGV.Sprint3.Task3.V15.Lib;
+
+namespace Tyuiu.GorovenkoGV.Sprint3.Task3.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string value = "lrmmse mg sermmmrt";
+            char item = 'm';
+
+            double res = ds.GetMinCharCount(value, item);
+
+            int wait = 6;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.GorovenkoGV.Sprint3.Task3.V15/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GorovenkoGV.Sprint3.Task3.V15.Lib;
+
+namespace Tyuiu.GorovenkoGV.Sprint3.Task3.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Горовенко Г.В. | ПКТб 23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнил: Горовенко Глеб Васильевич | ПКТб-23-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Используя цикл foreach подсчитать минимальное количество букв m         *");
+            Console.WriteLine("* находящихся на соседних позициях в строке: lrmmse mg sermmmrt           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("***************************************************************************");
+
+            string value = "lrmmse mg sermmmrt";
+            char chr = 'm';
+
+            Console.WriteLine("Исходный строка = " + value);
+            Console.WriteLine("Исходный символ = " + chr);
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Количество минимальных символов = " + ds.GetMinCharCount(value, chr));
+
+
+            Console.ReadKey();
+        }
+    }
+}
